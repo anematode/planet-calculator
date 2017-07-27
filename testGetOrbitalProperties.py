8fcf69524df25ddaffe188edae2bb19daea78034
anematode <timothy.herchen@gmail.com>
2017-07-26 21:07:26 -0700

    Update testGetOrbitalProperties.py

--- testGetOrbitalProperties.py
+++ testGetOrbitalProperties.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib2,math
 
 f = urllib2.urlopen('https://raw.githubusercontent.com/anematode/planet-calculator/master/mercurytest.dat')
 data = f.read()
@@ -12,10 +12,11 @@ Wlist = []
 MAlist = []
 Alist = []
 Tlist = []
+Ylist = []
 
 for i in xrange(1,len(splitData),5):
   currLine = splitData[i-1]
-  Tlist.append(float(currLine[:10])-2451545.0)
+  Tlist.append(float(currLine[:10]))
   currLine = splitData[i]
   EClist.append(float(currLine[5:26]))
   INlist.append(float(currLine[57:78]))
@@ -26,4 +27,32 @@ for i in xrange(1,len(splitData),5):
   MAlist.append(float(currLine[31:52]))
   currLine = splitData[i+3]
   Alist.append(float(currLine[5:26]))
-  
+  Ylist.append(float(currLine[31:52]))
+
+data = []
+
+degToRad = math.pi/180
+
+for epoch in xrange(0,len(Tlist),2):
+    startTime = Tlist[epoch]
+    endTime = Tlist[epoch+1]
+    
+    data.append(Alist[epoch])
+    data.append((Alist[epoch+1]-Alist[epoch])/16384)
+    
+    data.append(EClist[epoch])
+    data.append((EClist[epoch+1]-EClist[epoch])/16384)
+    
+    data.append(INlist[epoch] * degToRad)
+    data.append((INlist[epoch+1]-INlist[epoch])/16384 * degToRad)
+    
+    data.append(OMlist[epoch] * degToRad)
+    data.append((OMlist[epoch+1]-OMlist[epoch])/16384 * degToRad)
+    
+    data.append(MAlist[epoch] * degToRad)
+    data.append(32768 / Ylist[epoch] * 2 * math.pi)
+    
+    data.append(Wlist[epoch] * degToRad);
+    data.append((Wlist[epoch+1] - Wlist[epoch])/16384 * degToRad);
+
+print data
